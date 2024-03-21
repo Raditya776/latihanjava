86d1b08c1d644cde1534745530a418c6fe93d175
Raditya776 <adityabaledo802@gmail.com>
2024-03-21 17:04:09 +0700

    praktikum2

---
+++ Latihanvar.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanvar;
+
+/**
+ *
+ * @author Tri Wahyu
+ */
+public class Latihanvar {
+
+    public static void main(String[] args) {
+        // code aplecation
+        int JariJari;
+        double Keliling;
+        final double PI=3.14;
+        
+        JariJari =30;
+        Keliling =2*PI*JariJari;
+        System.out.println("Keliling Lingkran : " + Keliling);
+        System.out.println("=========");
+               
+              
+    }
+}
+
+
+
+
+
+
