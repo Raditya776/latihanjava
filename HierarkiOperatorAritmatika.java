e3dcd815e2cd7903f665b454e7c4963c3480f6e6
Raditya776 <adityabaledo802@gmail.com>
2024-03-27 19:25:34 +0700

    Praktikum4

---
+++ HierarkiOperatorAritmatika.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hierarkioperatoraritmatika;
+
+/**
+ *
+ * @author Tri Wahyu
+ */
+public class HierarkiOperatorAritmatika {
+
+    public static void main(String[] args) {
+        int A, B;
+        A = 8 + 2 + 3 / 6;
+        B = (8 + 2) * 3 / 6;
+        System.out.println("hasil dari A = "+ A);
+         System.out.println("hasil dari B = "+ B);
+        
+    }
+}
