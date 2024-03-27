dfb18a751412643aec71b06fa345e4e6249cb118
Raditya776 <adityabaledo802@gmail.com>
2024-03-27 19:41:11 +0700

    Praktikum5

---
+++ Operatorlogika.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operatorlogika;
+
+/**
+ *
+ * @author Tri Wahyu
+ */
+public class Operatorlogika {
+
+    public static void main(String[] args) {
+        int i = 9;
+        int j = 4;
+        
+        System.out.println(" Nilai variabel : ");
+        System.out.println(" i = "+1);
+        System.out.println( " j = "+j);
+        
+          System.out.println( " Penjumlahan : ");
+        System.out.println( " i + j = "+(i+j));
+        
+        System.out.println( " Pengurangan : ");
+        System.out.println( " i - j = "+(i-j));
+        
+        System.out.println( " Perkalian : ");
+        System.out.println( " i * j = "+(i*j));
+        
+        System.out.println( " Pembagian : ");
+        System.out.println( " i / j = "+(i/j));
+        
+        System.out.println( " Modulus : ");
+        System.out.println( " i % j = "+(i%j));
+        
+    }
+}
