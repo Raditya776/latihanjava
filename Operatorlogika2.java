bd1c993ac23b85e9a7df5e3fd87a7c73324cef31
Raditya776 <adityabaledo802@gmail.com>
2024-03-27 19:52:41 +0700

    Praktikum6

---
+++ Operatorlogika2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operatorlogika2;
+
+/**
+ *
+ * @author Tri Wahyu
+ */
+public class Operatorlogika2 {
+
+    public static void main(String[] args) {
+        System.out.println("operasi AND");
+        System.out.println("true && true = "+ (true && true));
+        System.out.println("true && false = "+ (true && false));
+        System.out.println("false && true = "+ (false && true));
+        System.out.println("false && false = "+ (false && false));
+        
+        System.out.println("\noperasi OR");
+        System.out.println("true || true = "+ (true || true));
+        System.out.println("true || false = "+ (true || false));
+        System.out.println("false || true = "+ (false || true));
+        System.out.println("false || false = "+ (false || false));
+        
+        System.out.println("\noperasi XOR");
+        System.out.println("true ^ true = "+ (true ^ true));
+        System.out.println("true ^ false = "+ (true ^ false));
+        System.out.println("false ^ true = "+ (false ^ true));
+        System.out.println("false ^ false = "+ (false ^ false));
+        
+        System.out.println("\noperasi NOT");
+        System.out.println("!true = "+ (!true));
+        System.out.println("!false = "+ (!false));
+
+        
+        
+    }
+}
